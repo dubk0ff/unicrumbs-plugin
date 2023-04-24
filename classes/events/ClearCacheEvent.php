20a80aff1378d654e5a25c6f49d53c94594976f0
dubk0ff <dubkov1401@gmail.com>
2023-04-24 17:04:34 +0300

    Cache clearing bug fix.

--- classes/events/ClearCacheEvent.php
+++ classes/events/ClearCacheEvent.php
@@ -2,11 +2,12 @@
 
 use Cms\Classes\EditorExtension;
 use Cms\Classes\Page;
+use Cms\Classes\Partial;
 use Dubk0ff\UniCrumbs\Classes\Helpers\CacheHelper;
 
 final class ClearCacheEvent
 {
-    public function __invoke(EditorExtension $controller, Page $templateObject, string $type): void
+    public function __invoke(EditorExtension $controller, Page|Partial $templateObject, string $type): void
     {
         if ($templateObject->hasComponent('unicrumbs')) {
             CacheHelper::clearCache(array_get($templateObject->unicrumbs, 'uuid', ''));
